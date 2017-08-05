559d18ec45a2460dde9f0c811a50f29110180010
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-08-05 01:08:02 -0300

    Add meta files & settings

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+const config = {
+  presets: [
+    [
+      'env',
+      {
+        modules: false,
+        browsers: ['IE >= 11', '> 5%']
+      }
+    ]
+  ]
+};
+
+module.exports = config;
---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const config = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: module
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
+  }
+};
+
+module.exports = config;
